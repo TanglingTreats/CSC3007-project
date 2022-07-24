f6043377a6eff9ba04cad417e4e4f6422dcfcf3c
Edwin Tang <edwintang10@gmail.com>
2022-07-24 15:26:35 +0800

    Add SVG and h3 with span. Add getting most recent data.

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-const reader = new FileReader();
 const target = "Russia-Ukraine Equipment Losses - Original.csv";
+const reader = new FileReader();
+
+const dateSpan = document.getElementById("current-date");
+
+function formatDate(date) {
+  const options = {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  };
+
+  return date.toLocaleDateString("en-SG", options);
+}
+
+function displayDate(date) {
+  console.log(date);
+  dateSpan.innerText = date;
+}
 
 (async () => {
   const res = await fetch(target, {
@@ -9,9 +26,16 @@ const target = "Russia-Ukraine Equipment Losses - Original.csv";
     },
   });
 
+  // Get CSV string from response
   const csvString = await res.text();
 
+  // Get JSON from csv string
   const data = d3.csvParse(csvString);
 
   console.log(data);
+  const latestSet = data[data.length - 1];
+
+  console.log(latestSet);
+
+  displayDate(formatDate(new Date(latestSet.Date)));
 })();
